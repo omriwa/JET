2227c70ff613f0c59084fee59c17d6524dd98280
Omri Wallach <omriwallach1992@gmail.com>
2023-08-24 16:26:45 +0300

    refactored fe, controller reset on disconnect and winning

--- src/controller.js
+++ src/controller.js
@@ -1,45 +1,59 @@
 
 const controller = (socket, gameData) => {
-    const emitValueByTurn = () => socket.to(gameData.turn).emit("value", gameData.value)
-    
-socket.on('connection', socket => {
-  console.log('user connected');
-
-  if (!gameData.player1) {
-    gameData.player1 = socket.id
-  }
-  else {
-    gameData.player2 = socket.id
-  }
-
-  if (gameData.player1 && gameData.player2) {
-    gameData.value = Math.ceil(Math.random() * 100)
-    gameData.turn = gameData.player2
-    
-    emitValueByTurn()
-  }
-
-  socket.on("play", value => {
-    gameData.value = Math.ceil((gameData.value + value) / 3)
-    gameData.turn = gameData.turn === gameData.player1 ? gameData.player2 : gameData.player1
-
-    emitValueByTurn()
-  })
-
-  socket.on('disconnect', socket => {
-    console.log('user disconnected');
-
-    if (gameData.player1 === socket.id) {
-      gameData.player1 = null
+    const emitValueByTurn = () => {
+        socket.to(gameData.turn).emit("value", { value: gameData.value, won: gameData.winningPlayer })
     }
-    else {
-      gameData.player2 = null
-    }
-    gameData.value = null
-  });
+    const resetGameForPlayer = () => {
+        gameData.value = null
+        gameData.winningPlayer = null
 
-  console.log("GAME DATA ", gameData.value)
-})
+        socket.emit("value", { value: gameData.value, won: gameData.winningPlayer })
+    }
+
+    socket.on('connection', socket => {
+        console.log('user connected');
+
+        if (!gameData.player1) {
+            gameData.player1 = socket.id
+        }
+        else {
+            gameData.player2 = socket.id
+        }
+
+        if (gameData.player1 && gameData.player2) {
+            gameData.value = Math.ceil(Math.random() * 100)
+            gameData.turn = gameData.player2
+
+            emitValueByTurn()
+        }
+        else {
+            resetGameForPlayer()
+        }
+
+        socket.on("play", value => {
+            gameData.value = Math.ceil((gameData.value + value) / 3)
+            gameData.turn = gameData.turn === gameData.player1 ? gameData.player2 : gameData.player1
+
+            if (gameData.value === 1) {
+                gameData.winningPlayer = true
+            }
+
+            emitValueByTurn()
+        })
+
+        socket.on('disconnect', socket => {
+            if (gameData.player1 === socket.id) {
+                gameData.player1 = null
+
+            }
+            else {
+                gameData.player2 = null
+            }
+
+            console.log('user disconnected');
+            resetGameForPlayer()
+        });
+    })
 }
 
 module.exports = { controller }
